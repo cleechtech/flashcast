98b95f39ace4c840a65a1d6b42bb22ccb0928097
Connor James Leech <connorleech@gmail.com>
2014-03-08 21:55:44 +0300

    add fetching json data

--- js/controllers/route.js
+++ js/controllers/route.js
@@ -1,9 +1,21 @@
 'use strict';
 
 app.controller('RouteCtrl', function($scope, Route){
-	Route.all(function(data){
-		$scope.routes = data
+
+	// routes.json file
+	Route.routes(function(data){
+		$scope.routes = data;
 	});
 
-	
+	// match.json file
+	Route.matches(function(data){
+		$scope.matches = data
+	});
+
+	// shapes.json file
+	Route.shapes(function(data){
+		$scope.shapes = data
+	});
+
 });
+
--- js/services/Route.js
+++ js/services/Route.js
@@ -2,14 +2,26 @@
 
 app.factory('Route', function($http){
 	var Route = {
-		all: function(callback){
+		routes: function(callback){
 			$http({
 				method: 'GET',
 				url:'data/routes.json'
 			}).success(callback)
 		},
+		matches: function(callback){
+			$http({
+				method: 'GET',
+				url:'data/match.json'
+			}).success(callback)
+		},
+		shapes: function(callback){
+			$http({
+				method: 'GET',
+				url:'data/shapes.json'
+			}).success(callback)
+		}
 	};
 	
 
 	return Route;
-})
+});
